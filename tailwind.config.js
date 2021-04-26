d3b9ae6978581e933ce021ba1acd9a0516bb85c3
André Roy <andreroy25.9@gmail.com>
2021-04-26 17:07:30 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const purge = process.env.NODE_ENV === "production" ? true : false;
+
+module.exports = {
+  purge: {
+    enable: purge,
+    content: ["./build/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat, sans-serif",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
