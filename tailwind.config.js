ef1a54d6c116a6e58daa5f94ddfb52d2eb93c3a7
André Roy <andreroy25.9@gmail.com>
2021-04-26 17:23:54 -0400

    Fix Purge Only in Production

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const purge = process.env.NODE_ENV === "production" ? true : false;
 
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: purge,
     content: ["./build/**/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
